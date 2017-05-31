d84215f44fcc80693994a5d0ecc2abdcdefb9def
John Sundell <john@sundell.co>
2017-05-31 23:27:15 +0200

    Re-organize code after adding handle support + improve documentation

--- Sources/ShellOut.swift
+++ Sources/ShellOut.swift
@@ -14,8 +14,8 @@ import Foundation
  *  - parameter command: The command to run
  *  - parameter arguments: The arguments to pass to the command
  *  - parameter path: The path to execute the commands at (defaults to current folder)
- *  - parameter outputHandle: The `FileHandle` where STDOUT should be redirected to
- *  - parameter errorHandle: The `FileHandle` where STDERR should be redirected to
+ *  - parameter outputHandle: Any `FileHandle` that any output (STDOUT) should be redirected to
+ *  - parameter errorHandle: Any `FileHandle` that any error output (STDERR) should be redirected to
  *
  *  - returns: The output of running the command
  *  - throws: `ShellOutError` in case the command couldn't be performed, or it returned an error
@@ -26,7 +26,8 @@ import Foundation
 @discardableResult public func shellOut(to command: String,
                                         arguments: [String] = [],
                                         at path: String = ".",
-                                       outputHandle: FileHandle? = nil, errorHandle: FileHandle? = nil) throws -> String {
+                                        outputHandle: FileHandle? = nil,
+                                        errorHandle: FileHandle? = nil) throws -> String {
     let process = Process()
     let command = "cd \"\(path)\" && \(command) \(arguments.joined(separator: " "))"
     return try process.launchBash(with: command, outputHandle: outputHandle, errorHandle: errorHandle)
@@ -37,8 +38,8 @@ import Foundation
  *
  *  - parameter commands: The commands to run
  *  - parameter path: The path to execute the commands at (defaults to current folder)
- *  - parameter outputHandle: The `FileHandle` where STDOUT should be redirected to
- *  - parameter errorHandle: The `FileHandle` where STDERR should be redirected to
+ *  - parameter outputHandle: Any `FileHandle` that any output (STDOUT) should be redirected to
+ *  - parameter errorHandle: Any `FileHandle` that any error output (STDERR) should be redirected to
  *
  *  - returns: The output of running the command
  *  - throws: `ShellOutError` in case the command couldn't be performed, or it returned an error
@@ -46,59 +47,48 @@ import Foundation
  *  Use this function to "shell out" in a Swift script or command line tool
  *  For example: `shellOut(to: ["mkdir NewFolder", "cd NewFolder"], at: "~/CurrentFolder")`
  */
-@discardableResult public func shellOut(to commands: [String], at path: String = ".", outputHandle: FileHandle? = nil, errorHandle: FileHandle? = nil) throws -> String {
+@discardableResult public func shellOut(to commands: [String],
+                                        at path: String = ".",
+                                        outputHandle: FileHandle? = nil,
+                                        errorHandle: FileHandle? = nil) throws -> String {
     let command = commands.joined(separator: " && ")
     return try shellOut(to: command, at: path, outputHandle: outputHandle, errorHandle: errorHandle)
 }
 
 // Error type thrown by the `shellOut()` function, in case the given command failed
 public struct ShellOutError: Swift.Error {
-    /// The buffer data retuned by `STDERR`.
-    public let stderr: Data
-    /// The buffer data retuned by `STOUT`.
-    public let stdout: Data
-    /// The termination status of the command.
+    /// The termination status of the command that was run
     public let terminationStatus: Int32
-    /// The error message that was returned through `STDERR`
-    public var message: String {
-        return stderr.shellOutput() ?? ""
-    }
-    /// Any output that was put in `STDOUT` despite the error being thrown
-    public var output: String {
-        return stdout.shellOutput() ?? ""
-    }
+    /// The error message as a UTF8 string, as returned through `STDERR`
+    public var message: String { return errorData.shellOutput() }
+    /// The raw error buffer data, as returned through `STDERR`
+    public let errorData: Data
+    /// The raw output buffer data, as retuned through `STDOUT`
+    public let outputData: Data
+    /// The output of the command as a UTF8 string, as returned through `STDOUT`
+    public var output: String { return outputData.shellOutput() }
 }
 
 // MARK: - Private
+
 private extension Process {
-
-    private func closeHandle(_ handle: FileHandle?) {
-        if let handle = handle {
-            handle.closeFile()
-        }
-    }
-
-    @discardableResult func launchBash(with command: String, outputHandle: FileHandle? = nil, errorHandle: FileHandle? = nil) throws -> Data {
+    @discardableResult func launchBash(with command: String, outputHandle: FileHandle? = nil, errorHandle: FileHandle? = nil) throws -> String {
         launchPath = "/bin/bash"
         arguments = ["-c", command]
 
-        var stdoutData = Data()
-        var stderrData = Data()
+        var outputData = Data()
+        var errorData = Data()
 
         let stdoutHandler: (FileHandle) -> Void = { handler in
             let data = handler.availableData
-            stdoutData.append(data)
-            if let handler = outputHandle {
-               handler.write(data)
-            }
+            outputData.append(data)
+            outputHandle?.write(data)
         }
 
         let stderrHandler: (FileHandle) -> Void = { handler in
             let data = handler.availableData
-            stderrData.append(data)
-            if let handler = errorHandle {
-                handler.write(data)
-            }
+            errorData.append(data)
+            errorHandle?.write(data)
         }
 
         let outputPipe = Pipe()
@@ -110,33 +100,30 @@ private extension Process {
         errorPipe.fileHandleForReading.readabilityHandler = stderrHandler
 
         launch()
-
         waitUntilExit()
 
-        // This is needed to close the file handle at the end. See: `Pipe` documentation
-        closeHandle(outputHandle)
-        closeHandle(errorHandle)
-
-        if terminationStatus != 0 {
-            throw ShellOutError(stderr: stderrData, stdout: stdoutData, terminationStatus: terminationStatus)
-        }
+        outputHandle?.closeFile()
+        errorHandle?.closeFile()
 
         FileHandle.standardError.readabilityHandler = nil
         FileHandle.standardOutput.readabilityHandler = nil
 
-        return stdoutData
-    }
+        if terminationStatus != 0 {
+            throw ShellOutError(
+                terminationStatus: terminationStatus,
+                errorData: errorData,
+                outputData: outputData
+            )
+        }
 
-    @discardableResult func launchBash(with command: String, outputHandle: FileHandle? = nil, errorHandle: FileHandle? = nil) throws -> String {
-        return try launchBash(with: command, outputHandle: outputHandle, errorHandle: errorHandle).shellOutput() ?? ""
+        return outputData.shellOutput()
     }
-
 }
 
 private extension Data {
-    func shellOutput() -> String? {
+    func shellOutput() -> String {
         guard let output = String(data: self, encoding: .utf8) else {
-            return nil
+            return ""
         }
 
         guard !output.hasSuffix("\n") else {

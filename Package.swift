1001ba1e498086770f63a10667175f40178f51e8
John Sundell <john@sundell.co>
2017-11-04 19:03:17 +0100

    Update project to Swift 4
    - Use the new Swift Package Description API.
    - Use the new substring API.

--- Package.swift
+++ Package.swift
@@ -1,7 +1,26 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
+
+/**
+ *  ShellOut
+ *  Copyright (c) John Sundell 2017
+ *  Licensed under the MIT license. See LICENSE file.
+ */
 
 import PackageDescription
 
 let package = Package(
-    name: "ShellOut"
+    name: "ShellOut",
+    products: [
+        .library(name: "ShellOut", targets: ["ShellOut"])
+    ],
+    targets: [
+        .target(
+            name: "ShellOut",
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "ShellOutTests",
+            dependencies: ["ShellOut"]
+        )
+    ]
 )

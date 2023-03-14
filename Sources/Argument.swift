a67ebf574a1bb62a75f4077a69fe952a7a84e0b0
Sven A. Schmidt <sas@finestructure.co>
2023-03-14 12:06:16 +0100

    Remove unused Argument.safe

--- Sources/Argument.swift
+++ Sources/Argument.swift
@@ -1,15 +1,6 @@
 public enum Argument {
-    case safe(SafeString)
     case quoted(QuotedString)
     case verbatim(String)
-
-    init(safe string: String) throws {
-        self = try .safe(.init(string))
-    }
-
-    init(_ string: SafeString) {
-        self = .safe(string)
-    }
 
     init(quoted string: String) {
         self = .quoted(.init(string))
@@ -21,8 +12,6 @@ public enum Argument {
 
     var string: String {
         switch self {
-            case let .safe(value):
-                return value.value
             case let .quoted(value):
                 return value.quoted
             case let .verbatim(string):
@@ -40,9 +29,5 @@ extension String {
 
 extension Array<String> {
     var quoted: [Argument] { map(\.quoted) }
-
-    @available(*, deprecated)
-    func safe() throws -> [Argument] { try map(Argument.init(safe:)) }
-
     var verbatim: [Argument] { map(\.verbatim) }
 }

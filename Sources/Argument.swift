78f2fe4f08013b07f06d6dc3476c052da48db743
Sven A. Schmidt <sas@finestructure.co>
2023-03-15 07:12:52 +0100

    More publicity

--- Sources/Argument.swift
+++ Sources/Argument.swift
@@ -2,15 +2,15 @@ public enum Argument {
     case quoted(QuotedString)
     case verbatim(String)
 
-    init(quoted string: String) {
+    public init(quoted string: String) {
         self = .quoted(.init(string))
     }
 
-    init(verbatim string: String) {
+    public init(verbatim string: String) {
         self = .verbatim(string)
     }
 
-    var string: String {
+    public var string: String {
         switch self {
             case let .quoted(value):
                 return value.quoted

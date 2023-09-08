1d78e620b42194d6b6b5bb7f409bef2648be303e
Sven A. Schmidt <sas@finestructure.co>
2023-09-08 09:12:06 +0200

    Move ShellOutCommand to own file

--- Sources/ShellOut.swift
+++ Sources/ShellOut.swift
@@ -91,41 +91,6 @@ import Algorithms
     )
 }
 
-/// Structure used to pre-define commands for use with ShellOut
-public struct ShellOutCommand {
-    /// The string that makes up the command that should be run on the command line
-    public var command: String
-
-    public var arguments: [String]
-
-    /// Initialize a value using a string that makes up the underlying command
-    public init(command: String, arguments: [String] = []) {
-        self.command = command
-        self.arguments = arguments
-    }
-
-    public func appending(arguments newArguments: [String]) -> Self {
-        .init(command: command, arguments: arguments + newArguments)
-    }
-
-    public func appending(argument: String) -> Self {
-        appending(arguments: [argument])
-    }
-
-    public mutating func append(arguments newArguments: [String]) {
-        self.arguments = self.arguments + newArguments
-    }
-
-    public mutating func append(argument: String) {
-        append(arguments: [argument])
-    }
-}
-
-extension ShellOutCommand: CustomStringConvertible {
-    public var description: String {
-        ([command] + arguments).joined(separator: " ")
-    }
-}
 
 public extension ShellOutCommand {
     static func bash(arguments: [Argument]) -> Self {
